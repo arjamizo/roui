70ba6b31b3ae10781a64f274eeb1065fdf50678e
dev <dev@localhost>
2023-10-25 01:33:44 +0200

    feat: implement input props logic (label, required, info)

--- lib/src/components/Input.vue
+++ lib/src/components/Input.vue
@@ -1,18 +1,49 @@
 <template>
-  <input/>
+  <div class="flex flex-col">
+    <div class="flex flex-row label-cont">
+      <label v-if="label" class="flex flex-col">
+        <my-input v-bind="$attrs" v-on="$listeners" class="pr-[15px]"/>
+        <span>{{ label }}</span>
+      </label>
+      <my-input v-else class="pr-[15px]"/>
+      <span v-if="required" class="abs w-[0] h-[0]">
+        <div class="relative top-[8px] right-[12px] text-[orange] text-[2rem]">*</div>
+      </span>
+    </div>
+    <div class="flex flex-row" v-if="info">
+      (i){{ ' ' }}
+      {{ info }}
+    </div>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
+const MyInput = defineComponent({
+  template: `<input type="text" v-bind="$attrs" :placeholder="$attrs.label" v-on="$listeners"/>`,
+})
+
 export const RouiInput = defineComponent({
+  name: 'roui-input',
+  inheritAttrs: false,
+  props: ['info', 'required', 'label'],
+  components: {
+    MyInput,
+  },
 })
 
 export default RouiInput
 </script>
 
 <style>
-input {
-  border: 1px solid black;
-}
+  input[type='text'] {
+    border-bottom: 2px solid grey;
+    margin: 2px;
+  }
+
+  input[required="required"]::after {
+    content:" *";
+    color: grey;
+  }
 </style>
