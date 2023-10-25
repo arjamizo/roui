056338e25cd7d76db3222d1dbc21afb9badcdd99
AZ <1611803+arjamizo@users.noreply.github.com>
2023-10-25 18:00:18 +0200

    feat: init vite vue3 app

---
+++ lib/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// import RouiInput from './components/Input.vue'
+
+const name = ref('Imię i nazwisko')
+
+</script>
+
+<template>
+  <input v-model="name"/>
+  <!-- <roui-input v-model="name"/> -->
+  <span>{{ name }}</span>
+</template>
+
+<style scoped>
+</style>
