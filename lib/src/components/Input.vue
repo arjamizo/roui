96d5581b2c767b886b1e9a8266938ebc624af319
Kl4y4 <surowiec.bartosz@wp.pl>
2023-10-31 19:33:52 +0100

    Styled input

--- lib/src/components/Input.vue
+++ lib/src/components/Input.vue
@@ -2,16 +2,23 @@
   <div class="flex flex-col">
     <div class="flex flex-row label-cont">
       <label v-if="label" class="flex flex-col">
-        <my-input v-bind="$attrs" v-on="$listeners" class="pr-[15px]"/>
-        <span>{{ label }}</span>
+        <my-input v-bind="$attrs" v-on="$listeners" class="peer pr-[15px] w-96 h-10 bg-white text-gray-400 text-xl focus:outline-none font-medium placeholder-transparent"/>
+        <span class="absolute left-3 -top-2 text-base text-yellow-500
+          transition-all
+          peer-placeholder-shown:top-4
+          peer-placeholder-shown:text-xl
+          peer-focus:-top-2
+          peer-focus:text-base
+        "
+        >{{ label }}</span>
       </label>
-      <my-input v-else class="pr-[15px]"/>
+      <my-input v-else class="pr-[15px] w-96 h-10 bg-white text-gray-400 text-lg focus:outline-none font-medium"/>
       <span v-if="required" class="abs w-[0] h-[0]">
-        <div class="relative top-[8px] right-[12px] text-[orange] text-[2rem]">*</div>
+        <div class="relative top-[8px] right-[12px] text-[orange] text-3xl">*</div>
       </span>
     </div>
     <div class="flex flex-row" v-if="info">
-      <span class="material-icons info text-[#bfbfbf]">info</span>
+      <span class="material-icons info text-gray-300">info</span>
       {{ info }}
     </div>
   </div>
@@ -38,7 +45,7 @@ export default RouiInput
 
 <style>
   input[type='text'] {
-    border-bottom: 2px solid grey;
+    border-bottom: 2px solid orange;
     margin: 2px;
   }
 
