07d800e1707ca3c25da7ee09fb52416d00c66e9f
Kl4y4 <surowiec.bartosz@wp.pl>
2023-11-03 16:13:44 +0100

    Fixed label positioning

--- lib/src/components/Input.vue
+++ lib/src/components/Input.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col py-3 pr-8">
     <div class="flex flex-row label-cont">
-      <label v-if="label" class="flex flex-col">
-        <my-input v-bind="$attrs" v-on="$listeners" class="peer pr-[15px] w-96 h-10 bg-white text-gray-400 text-xl focus:outline-none font-medium placeholder-transparent"/>
-        <span class="absolute left-3 -top-2 text-base text-yellow-500
+      <label v-if="label" class="flex flex-col relative">
+        <my-input v-bind="$attrs" v-on="$listeners" class="peer pr-[15px] w-96 h-10 bg-white text-gray-500 text-lg focus:outline-none font-medium placeholder-transparent"/>
+        <span class="absolute left-1 -top-2 text-xs text-yellow-500 font-medium
           transition-all
-          peer-placeholder-shown:top-4
+          peer-placeholder-shown:top-2
           peer-placeholder-shown:text-xl
           peer-focus:-top-2
-          peer-focus:text-base
+          peer-focus:text-xs
         "
         >{{ label }}</span>
       </label>
-      <my-input v-else class="pr-[15px] w-96 h-10 bg-white text-gray-400 text-lg focus:outline-none font-medium"/>
+      <my-input v-else class="pr-[15px] w-96 h-10 bg-white text-gray-500 text-lg focus:outline-none font-medium"/>
       <span v-if="required" class="abs w-[0] h-[0]">
-        <div class="relative top-[8px] right-[12px] text-[orange] text-3xl">*</div>
+        <div class="relative top-1 right-4 text-yellow-500 text-3xl font-medium">*</div>
       </span>
     </div>
-    <div class="flex flex-row" v-if="info">
-      <span class="material-icons info text-gray-300">info</span>
-      {{ info }}
+    <div class="flex flex-row gap-2 text-xs items-center -mg-top-2" v-if="info">
+      <span class="material-icons info text-gray-500">info</span>
+      <span class="text-gray-500">{{ info }}</span>
     </div>
   </div>
 </template>
